5c23ed43d8f7f21c57b7f415b444a5d66a827df8
xili22 <xiaozhou.li@oulu.fi>
2024-10-31 17:53:54 +0200

    User Story 7

--- piglatin.py
+++ piglatin.py
@@ -1,3 +1,5 @@
+from error import PigLatinError
+
 
 class PigLatin:
     def __init__(self, phrase: str):
@@ -7,27 +9,58 @@ class PigLatin:
         return self.phrase
 
     def translate(self) -> str:
-        if self.phrase == '':
+
+        if not self.phrase:
             return 'nil'
+
         words = self.phrase.split()
         translated_words = []
+
         for word in words:
-            parts = word.split('-')
-            translated_parts = []
-            for part in parts:
-                if part[0] in 'aeiouAEIOU':
-                    if part[-1] in 'aeiouAEIOU':
-                        translated_parts.append(part + 'yay')
-                    elif part[-1] == 'y':
-                        translated_parts.append(part + 'nay')
-                    else:
-                        translated_parts.append(part + 'ay')
-                else:
-                    first_vowel_idx = next((i for i, letter in enumerate(part) if letter in 'aeiouAEIOU'), None)
-                    if first_vowel_idx is None or first_vowel_idx == 0:
-                        translated_parts.append(part[1:] + part[0] + 'ay')
-                    else:
-                        translated_parts.append(part[first_vowel_idx:] + part[:first_vowel_idx] + 'ay')
-            translated_words.append('-'.join(translated_parts))
+            # Handle punctuation
+            prefix_punct = ''
+            suffix_punct = ''
+            core_word = word
+
+            # Extract prefix punctuation
+            while core_word and not core_word[0].isalnum():
+                if core_word[0] not in [",",".",";",":","'","?","!","(",")"]:
+                    raise PigLatinError
+
+                prefix_punct += core_word[0]
+                core_word = core_word[1:]
+
+            # Extract suffix punctuation
+            while core_word and not core_word[-1].isalnum():
+                if core_word[-1] not in [",",".",";",":","'","?","!","(",")"]:
+                    raise PigLatinError
+
+                suffix_punct = core_word[-1] + suffix_punct
+                core_word = core_word[:-1]
+
+            # Translate the core word
+            if core_word:
+                translated_word = self.translate_word(core_word)
+            else:
+                translated_word = ''
+
+            # Reassemble the word with punctuation
+            translated_words.append(prefix_punct + translated_word + suffix_punct)
+
         return ' '.join(translated_words)
+
+    def translate_word(self, word: str) -> str:
+        if word[0].lower() in 'aeiou':
+            if word[-1] in 'aeiou':
+                return word + 'yay'
+            elif word[-1] == 'y':
+                return word + 'nay'
+            else:
+                return word + 'ay'
+        else:
+            first_vowel_idx = next((i for i, letter in enumerate(word) if letter.lower() in 'aeiou'), None)
+            if first_vowel_idx is None:
+                return word + 'ay'
+            else:
+                return word[first_vowel_idx:] + word[:first_vowel_idx] + 'ay'
 
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -6,37 +6,62 @@ from error import PigLatinError
 class TestPigLatin(unittest.TestCase):
 
     def test_input_phrase(self):
-        translator = PigLatin.PigLatin('This is a phrase')
+        translator = PigLatin('This is a phrase')
         self.assertEqual(translator.get_phrase(),'This is a phrase')
 
     def test_empty_str_nil(self):
-        translator = PigLatin.PigLatin('')
+        translator = PigLatin('')
         self.assertEqual(translator.translate(),'nil')
 
     def test_start_vowel_end_y(self):
-        translator = PigLatin.PigLatin('any')
+        translator = PigLatin('any')
         self.assertEqual(translator.translate(),'anynay')
 
     def test_start_vowel_end_consonant(self):
-        translator = PigLatin.PigLatin('ask')
+        translator = PigLatin('ask')
         self.assertEqual(translator.translate(),'askay')
 
     def test_start_vowel_end_vowel(self):
-        translator = PigLatin.PigLatin('apple')
+        translator = PigLatin('apple')
         self.assertEqual(translator.translate(),'appleyay')
 
     def test_start_consonant(self):
-        translator = PigLatin.PigLatin('hello')
+        translator = PigLatin('hello')
         self.assertEqual(translator.translate(),'ellohay')
 
     def test_start_mult_consonants(self):
-        translator = PigLatin.PigLatin('known')
+        translator = PigLatin('known')
         self.assertEqual(translator.translate(),'ownknay')
 
     def test_multiple_words(self):
-        translator = PigLatin.PigLatin('hello world')
+        translator = PigLatin('hello world')
         self.assertEqual(translator.translate(),'ellohay orldway')
 
     def test_composite_words(self):
-        translator = PigLatin.PigLatin('well-being')
+        translator = PigLatin('well-being')
         self.assertEqual(translator.translate(),'ellway-eingbay')
+
+    # .,;:'?!()
+    def test_punctuation(self):
+        translator = PigLatin("hello world!")
+        self.assertEqual(translator.translate(),"ellohay orldway!")
+
+    def test_parenthesis(self):
+        translator = PigLatin("(hello world)")
+        self.assertEqual(translator.translate(),"(ellohay orldway)")
+
+    def test_punctuation_2(self):
+        translator = PigLatin("hello world, nice day.")
+        self.assertEqual(translator.translate(),"ellohay orldway, icenay ayday.")
+
+    def test_punctuation_3(self):
+        translator = PigLatin("hello world; it's a nice day:")
+        self.assertEqual(translator.translate(),"ellohay orldway; it'say ayay icenay ayday:")
+
+    def test_punctuation_between(self):
+        translator = PigLatin("hello world! how are you?")
+        self.assertEqual(translator.translate(),"ellohay orldway! owhay areyay ouyay?")
+
+    def test_unallowed_punctuation(self):
+        translator = PigLatin('test% and€')
+        self.assertRaises(PigLatinError,translator.translate)
